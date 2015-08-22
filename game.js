71b64fe6e2cae1e725883aa335893422dc01de2d
Kevin Michel <kmichel.info@gmail.com>
2015-08-22 21:25:47 +0200

    Add basic logic for exit door

--- game.js
+++ game.js
@@ -28,7 +28,10 @@ var Game = {
     random: function (min, max) {
         return min + Math.random() * (max - min)
     },
-    makeTriangle: function (cellX, cellY, level, markOccupied) {
+    randomPick: function (list) {
+        return list[Math.floor(Math.random() * list.length)];
+    },
+    makeTriangle: function (cellX, cellY, level, colors, markOccupied) {
         var x = (cellX + 0.5) * level.cellSize;
         var y = (cellY + 0.5) * level.cellSize;
         var triangle = {
@@ -40,17 +43,17 @@ var Game = {
             bottom: {
                 x: 0, y: -31 + 8, angle: Game.random(-0.03, 0.03), scale: 1,
                 transform: undefined,
-                color: [Math.random(), Math.random(), Math.random()]
+                color: Game.randomPick(Object.keys(colors))
             },
             middle: {
                 x: 0, y: 8, angle: Game.random(-0.05, 0.05), scale: 1,
                 transform: undefined,
-                color: [Math.random(), Math.random(), Math.random()]
+                color: Game.randomPick(Object.keys(colors))
             },
             top: {
                 x: 0, y: 35 + 8, angle: Game.random(-0.07, 0.07), scale: 1,
                 transform: undefined,
-                color: [Math.random(), Math.random(), Math.random()]
+                color: Game.randomPick(Object.keys(colors))
             }
         };
         if (markOccupied)
@@ -89,6 +92,8 @@ var Game = {
             textures: {
                 ground: undefined,
                 wall: undefined,
+                exit_closed: undefined,
+                exit_open: undefined,
                 triangle_bottom_mask: undefined,
                 triangle_middle_mask: undefined,
                 triangle_top_mask: undefined,
@@ -115,40 +120,49 @@ var Game = {
                 swapNpc: undefined
             },
             player: undefined,
-            npcs: []
-
+            npcs: [],
+            colors: {
+                red: [0.6, 0.1, 0.1],
+                green: [0.2, 0.4, 0.7],
+                blue: [0.2, 0.7, 0.1]
+            }
         };
 
         // Build a random "level" for now
         state.level.centerX = 0.5 * state.level.width * state.level.cellSize;
         state.level.centerY = 0.5 * state.level.height * state.level.cellSize;
         state.level.cells = [];
-        for (var y = state.level.height - 1; y >= 0; --y) {
-            for (var x = 0; x < state.level.width; ++x) {
-                var cell = {
-                    type: ((x + y * 2) % 5) < 2 ? "wall" : "ground",
-                    x: (x + 0.5) * state.level.cellSize,
-                    y: (y + 0.5) * state.level.cellSize,
-                    angle: 0,
-                    scale: 1,
-                    transform: undefined,
-                    cellX: x,
-                    cellY: y,
-                    occupiedBy: undefined
-                };
-                cell.transform = GG.Transforms.createTRS(cell.x, cell.y, cell.angle, cell.scale);
-                state.level.cells.push(cell);
-            }
+
+        function createCell(x, y, type) {
+            var cell = {
+                type: type,
+                x: (x + 0.5) * state.level.cellSize,
+                y: (y + 0.5) * state.level.cellSize,
+                angle: 0,
+                scale: 1,
+                transform: undefined,
+                cellX: x,
+                cellY: y,
+                occupiedBy: undefined,
+                isOpen: true
+            };
+            cell.transform = GG.Transforms.createTRS(cell.x, cell.y, cell.angle, cell.scale);
+            return cell;
         }
+
+        for (var y = state.level.height - 1; y >= 0; --y)
+            for (var x = 0; x < state.level.width; ++x)
+                state.level.cells.push(createCell(x, y, ((x + y * 2) % 5) < 2 ? "wall" : "ground"));
+        state.level.cells.push(createCell(-1, 4, "exit"));
         state.level.index = {};
         for (var i = 0; i < state.level.cells.length; ++i) {
-            cell = state.level.cells[i];
+            var cell = state.level.cells[i];
             state.level.index[cell.cellX + "_" + cell.cellY] = cell;
         }
 
-        state.npcs.push(Game.makeTriangle(5, 2, state.level, true));
-        state.npcs.push(Game.makeTriangle(3, 3, state.level, true));
-        state.player = Game.makeTriangle(3, 2, state.level, false);
+        state.npcs.push(Game.makeTriangle(5, 2, state.level, state.colors, true));
+        state.npcs.push(Game.makeTriangle(3, 3, state.level, state.colors, true));
+        state.player = Game.makeTriangle(3, 2, state.level, state.colors, false);
 
         state.camera.targetX = state.level.centerX;
         state.camera.targetY = state.level.centerY;
@@ -192,6 +206,34 @@ var Game = {
             if (Math.abs(player.x - player.targetX) < 2 && Math.abs(player.y - player.targetY) < 2) {
                 player.x = player.targetX;
                 player.y = player.targetY;
+                var neighbors = [
+                    {x: player.cellX - 1, y: player.cellY},
+                    {x: player.cellX + 1, y: player.cellY},
+                    {x: player.cellX, y: player.cellY - 1},
+                    {x: player.cellX, y: player.cellY + 1}
+                ];
+                var isNearExit = false;
+                neighbors.forEach(function (neighbor) {
+                    var cellIndex = neighbor.x + "_" + neighbor.y;
+                    if (cellIndex in state.level.index) {
+                        var cell = state.level.index[cellIndex];
+                        if (cell.type === "exit") {
+                            isNearExit = true;
+                            state.camera.targetX = state.level.centerX * 0.9 + player.x * 0.1;
+                            state.camera.targetY = state.level.centerY * 0.9 + player.y * 0.1;
+                            state.camera.targetScale = 1280 / Game.config.canvasWidth;
+                            if (cell.isOpen) {
+                                if (player.bottom.color !== player.middle.color || player.middle.color !== player.top.color)
+                                    cell.isOpen = false;
+                            }
+                        }
+                    }
+                });
+                if (!isNearExit && state.level.state === "move") {
+                    state.camera.targetX = state.level.centerX;
+                    state.camera.targetY = state.level.centerY;
+                    state.camera.targetScale = 1280 / Game.config.canvasWidth;
+                }
                 if (player.nextMoves.length > 0) {
                     var nextMove = player.nextMoves[0];
                     var nextX = (nextMove.x + 0.5) * state.level.cellSize;
@@ -199,17 +241,25 @@ var Game = {
                     if (nextMove.occupiedBy !== undefined) {
                         state.camera.targetX = 0.5 * (player.x + nextX);
                         state.camera.targetY = 0.5 * (player.y + nextY);
-                        state.camera.targetScale = 472 / Game.config.canvasWidth;
+                        state.camera.targetScale = 700 / Game.config.canvasWidth;
                         state.level.state = "swap";
                         state.level.swapNpc = nextMove.occupiedBy;
                     } else {
-                        player.cellX = nextMove.x;
-                        player.cellY = nextMove.y;
-                        // TODO: make targetX/Y implicit ?
-                        player.targetX = nextX;
-                        player.targetY = nextY;
+                        if (nextMove.type !== "exit" || state.level.index[nextMove.x + "_" + nextMove.y].isOpen) {
+                            player.cellX = nextMove.x;
+                            player.cellY = nextMove.y;
+                            // TODO: make targetX/Y implicit ?
+                            player.targetX = nextX;
+                            player.targetY = nextY;
+                        }
                     }
                     player.nextMoves.splice(0, 1);
+
+                    state.level.cells.forEach(function (cell) {
+                        if (cell.type === "exit" && !cell.isOpen) {
+                            cell.isOpen = true;
+                        }
+                    });
                 }
             }
         }
@@ -232,20 +282,21 @@ var Game = {
                 movementY += 1;
                 keyboardInput.up = false;
             }
-            if (movementX != 0)
+            if (movementX !== 0)
                 movementY = 0;
-            if (movementX != 0 || movementY != 0) {
+            if (movementX !== 0 || movementY !== 0) {
                 var lastCell;
                 if (player.nextMoves.length > 0)
                     lastCell = player.nextMoves[player.nextMoves.length - 1];
                 else
-                    lastCell = {x: player.cellX, y: player.cellY, occupiedBy: undefined};
-                if (lastCell.occupiedBy === undefined) {
-                    var newCell = {x: lastCell.x + movementX, y: lastCell.y + movementY, occupiedBy: undefined};
+                    lastCell = {x: player.cellX, y: player.cellY, occupiedBy: undefined, type:undefined};
+                if (lastCell.occupiedBy === undefined || lastCell.type === "exit") { // TODO: fix microbug here on level end
+                    var newCell = {x: lastCell.x + movementX, y: lastCell.y + movementY, occupiedBy: undefined, type:undefined};
                     var newCellIndex = newCell.x + "_" + newCell.y;
                     if (state.level.index.hasOwnProperty(newCellIndex)) {
                         var cell = state.level.index[newCellIndex];
-                        if (cell.type == "ground") {
+                        newCell.type = cell.type;
+                        if (cell.type === "ground" || cell.type === "exit") {
                             newCell.occupiedBy = cell.occupiedBy;
                             player.nextMoves.push(newCell);
                         }
@@ -314,9 +365,9 @@ var Game = {
             updateTriangle(state.npcs[i]);
 
         var camera = state.camera;
-        camera.x = camera.targetX * 0.2 + camera.x * 0.8;
-        camera.y = camera.targetY * 0.2 + camera.y * 0.8;
-        camera.scale = camera.targetScale * 0.2 + camera.scale * 0.8;
+        camera.x = camera.targetX * 0.1 + camera.x * 0.9;
+        camera.y = camera.targetY * 0.1 + camera.y * 0.9;
+        camera.scale = camera.targetScale * 0.1 + camera.scale * 0.9;
     },
     draw: function (state) {
         var gl = state.gl;
@@ -347,9 +398,23 @@ var Game = {
             var model = new Float32Array(9);
 
             state.vaoExtension.bindVertexArrayOES(state.meshes.quad_128);
-            gl.bindTexture(gl.TEXTURE_2D, state.textures.ground);
             for (var i = 0; i < state.level.cells.length; ++i) {
                 var cell = state.level.cells[i];
+                if (cell.type == "exit") {
+                    if (cell.isOpen)
+                        gl.bindTexture(gl.TEXTURE_2D, state.textures.exit_open);
+                    else
+                        gl.bindTexture(gl.TEXTURE_2D, state.textures.exit_closed);
+                    GG.Transforms.toMat3(cell.transform, model);
+                    gl.uniformMatrix3fv(state.shader.uniforms.model, false, model);
+                    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+                }
+            }
+
+            state.vaoExtension.bindVertexArrayOES(state.meshes.quad_128);
+            gl.bindTexture(gl.TEXTURE_2D, state.textures.ground);
+            for (i = 0; i < state.level.cells.length; ++i) {
+                cell = state.level.cells[i];
                 if (cell.type == "ground") {
                     GG.Transforms.toMat3(cell.transform, model);
                     gl.uniformMatrix3fv(state.shader.uniforms.model, false, model);
@@ -377,12 +442,16 @@ var Game = {
         }
 
         function drawTriangle(triangle, highlightedPart) {
+            var bottomColor = state.colors[triangle.bottom.color];
+            var middleColor = state.colors[triangle.middle.color];
+            var topColor = state.colors[triangle.top.color];
+
             GG.Transforms.toMat3(triangle.bottom.transform, model);
             gl.uniformMatrix3fv(state.shader.uniforms.model, false, model);
             if (highlightedPart === "bottom")
-                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(triangle.bottom.color, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
+                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(bottomColor, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
             else
-                gl.uniform3fv(state.shader.uniforms.tint, triangle.bottom.color);
+                gl.uniform3fv(state.shader.uniforms.tint, bottomColor);
             gl.bindTexture(gl.TEXTURE_2D, state.textures.triangle_bottom_mask);
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
             gl.uniform3f(state.shader.uniforms.tint, 1, 1, 1);
@@ -392,9 +461,9 @@ var Game = {
             GG.Transforms.toMat3(triangle.middle.transform, model);
             gl.uniformMatrix3fv(state.shader.uniforms.model, false, model);
             if (highlightedPart === "middle")
-                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(triangle.middle.color, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
+                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(middleColor, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
             else
-                gl.uniform3fv(state.shader.uniforms.tint, triangle.middle.color);
+                gl.uniform3fv(state.shader.uniforms.tint, middleColor);
             gl.bindTexture(gl.TEXTURE_2D, state.textures.triangle_middle_mask);
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
             gl.uniform3f(state.shader.uniforms.tint, 1, 1, 1);
@@ -404,9 +473,9 @@ var Game = {
             GG.Transforms.toMat3(triangle.top.transform, model);
             gl.uniformMatrix3fv(state.shader.uniforms.model, false, model);
             if (highlightedPart === "top")
-                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(triangle.top.color, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
+                gl.uniform3fv(state.shader.uniforms.tint, Game.lerp3(topColor, [1, 1, 1], 0.5 + 0.25 * Math.sin(8 * state.time)));
             else
-                gl.uniform3fv(state.shader.uniforms.tint, triangle.top.color);
+                gl.uniform3fv(state.shader.uniforms.tint, topColor);
             gl.bindTexture(gl.TEXTURE_2D, state.textures.triangle_top_mask);
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
             gl.uniform3f(state.shader.uniforms.tint, 1, 1, 1);
