d68c790c5c0387bbee96ea9c8c53f3bf3f9a60c5
Kevin Michel <kmichel.info@gmail.com>
2015-08-23 20:31:46 +0200

    Make second level more interesting

--- levels/level_01.js
+++ levels/level_01.js
@@ -11,13 +11,18 @@ Game.Levels.push({
         {x: 4, y: 0, type: "ground"},
         {x: 5, y: 0, type: "ground"},
         {x: 6, y: 0, type: "ground"},
-        {x: 7, y: 0, type: "ground"}
+        {x: 7, y: 0, type: "ground"},
+        {x: 2, y: 1, type: "ground"},
+        {x: 3, y: 1, type: "wall"},
+        {x: 4, y: 1, type: "ground"},
+        {x: 5, y: 1, type: "ground"}
     ],
     player: {
         x: 7, y: 0
     },
     npcs: [
+        {x: 6, y: 0, bottom: "blue", middle: "green", top: "blue"},
         {x: 4, y: 0, bottom: "red", middle: "green", top: "red"},
-        {x: 2, y: 0, bottom: "blue", middle: "green", top: "blue"}
+        {x: 2, y: 0, bottom: "green", middle: "blue", top: "green"}
     ]
 });
